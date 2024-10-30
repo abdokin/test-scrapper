6f6faf57533535677405dc22e5c01fd022ce8a85
abdokin <bashbin.sh@gmail.com>
2024-10-30 17:02:55 +0100

    Update data

--- merge_json_data.js
+++ merge_json_data.js
@@ -1,25 +1,59 @@
 const fs = require('fs');
 const { join } = require('path');
+
+function flipMap(map) {
+    const flipped = {};
+    for (const [key, value] of Object.entries(map)) {
+        flipped[value] = key;
+    }
+    return flipped;
+}
+
+const categories_map = {
+    cognitive_ability: 'Cognitive ability',
+    programming_skills: 'Programming skills',
+    role_specific_skills: 'Role-specific skills',
+    situational_judgment: 'Situational judgment',
+    software_skills: 'Software skills'
+};
+
 const category = parse_args();
-const dir = join(__dirname, 'tests', category);
 
-// Read the file
-const files = fs.readdirSync(dir);
+if (category) {
+    
+    mergeCategoryData(category);
+} else {
+    
+    Object.keys(categories_map).forEach(mergeCategoryData);
+}
 
-// Merge the data
-const data = files.map((file) => {
-    return JSON.parse(fs.readFileSync(join(dir, file), 'utf8'));
-});
+function mergeCategoryData(category) {
+    const dir = join(__dirname, 'tests', category);
 
-// Save output to a file
-fs.writeFileSync(join(__dirname, 'tests', `${category}.json`), JSON.stringify(data, null, 2));
+    if (!fs.existsSync(dir)) {
+        console.error(`Directory not found for category: ${category}`);
+        return;
+    }
 
+    const files = fs.readdirSync(dir);
 
+    const mergedData = files.map((file) => {
+        let fileData = JSON.parse(fs.readFileSync(join(dir, file), 'utf8'));
+        fileData['attributes']['category'] = flipMap(categories_map)[fileData['attributes']['category']];
+        return fileData;
+    });
+
+    const outputFilename = join(__dirname, 'gists', `${categories_map[category] || category}.json`);
+
+    
+    fs.writeFileSync(outputFilename, JSON.stringify(mergedData));
+    console.log(`Merged data saved to: ${outputFilename}`);
+}
 function parse_args() {
     const args = process.argv.slice(2);
-    if (args.length !== 1) {
-        console.error('Usage: node merge_json_data.js <category>');
+    if (args.length > 1) {
+        console.error('Usage: node merge_json_data.js [category]');
         process.exit(1);
     }
-    return args[0];
+    return args[0] || null; 
 }
--- validate_test_schema.js
+++ validate_test_schema.js
@@ -1,99 +1,112 @@
 const zod = require('zod');
 const fs = require('fs');
 const { join } = require('path');
-const fileName = parse_args();
-const file = fs.readFileSync(join(__dirname, 'tests', fileName));
-const data = JSON.parse(file);
 
-const schema = zod.array(
-    zod.object({
-        title: zod.string(),
-        attributes: zod.object({
-            overview: zod.string(),
-            level: zod.enum(['entry_level', 'intermediate', 'advanced']),
-            covered_skills: zod.array(zod.string()),
-            relevancy: zod.string(),
-            description: zod.string(),
-            category: zod.string(),
-            language: zod.enum(['french', 'english']),
-            duration_seconds: zod.number().int(),
-            position: zod.number().int(),
-            questions_to_answer: zod.number().int(),
-        }),
-        questions: zod.array(zod.object({
-            question_attributes: zod.object({
-                type: zod.enum(['multiple_choice', 'multiple_response']),
-                preview: zod.boolean().optional(),
-                content: zod.string(),
+const fileNames = parse_args();
+fileNames.forEach(fileName => {
+    const file = fs.readFileSync(join(__dirname, 'gists', fileName));
+    const data = JSON.parse(file);
+
+    const schema = zod.array(
+        zod.object({
+            title: zod.string(),
+            attributes: zod.object({
+                overview: zod.string(),
+                level: zod.enum(['entry_level', 'intermediate', 'advanced']),
+                covered_skills: zod.array(zod.string()),
+                relevancy: zod.string(),
+                description: zod.string(),
+                category: zod.enum([
+                    'cognitive_ability',
+                    'programming_skills',
+                    'role_specific_skills',
+                    'situational_judgment',
+                    'software_skills'
+                ]),
+                language: zod.enum(['french', 'english']),
+                duration_seconds: zod.number().int(),
+                position: zod.number().int(),
+                questions_to_answer: zod.number().int(),
             }),
-            options: zod.array(zod.object({
-                content: zod.string(),
-                correct: zod.boolean().optional(),
-            }))
-        }).superRefine((question, ctx) => {                
-            // ensure options are unique
-            const options = question.options.map(option => option.content);
-            if (new Set(options).size !== options.length) {
-                ctx.addIssue({
-                    message: 'Options must be unique',
-                    code: 'invalid_options',
-                });
-            }
-            // ensure multiple choice questions have only one correct answer
-            if (question.question_attributes.type === 'multiple_choice') {
-                const correct = question.options.filter(option => option.correct);
-                if (correct.length !== 1) {
+            questions: zod.array(zod.object({
+                question_attributes: zod.object({
+                    type: zod.enum(['multiple_choice', 'multiple_response']),
+                    preview: zod.boolean().optional(),
+                    content: zod.string(),
+                }),
+                options: zod.array(zod.object({
+                    content: zod.string(),
+                    correct: zod.boolean().optional(),
+                }))
+            }).superRefine((question, ctx) => {
+                // ensure options are unique
+                const options = question.options.map(option => option.content);
+                if (new Set(options).size !== options.length) {
                     ctx.addIssue({
-                        message: `Multiple choice questions must have exactly one correct answer for question "${question.question_attributes.content}"`,
-                        code: 'invalid_correct_option',
+                        message: 'Options must be unique',
+                        code: 'invalid_options',
                     });
                 }
-            }
-            // ensure has at least one correct answer for multiple response questions
-            if (question.question_attributes.type === 'multiple_response') {
-                const correct = question.options.filter(option => option.correct);
-                if (correct.length === 0) {
+                // ensure multiple choice questions have only one correct answer
+                if (question.question_attributes.type === 'multiple_choice') {
+                    const correct = question.options.filter(option => option.correct);
+                    if (correct.length !== 1) {
+                        ctx.addIssue({
+                            message: `Multiple choice questions must have exactly one correct answer for question "${question.question_attributes.content}"`,
+                            code: 'invalid_correct_option',
+                        });
+                    }
+                }
+                // ensure has at least one correct answer for multiple response questions
+                if (question.question_attributes.type === 'multiple_response') {
+                    const correct = question.options.filter(option => option.correct);
+                    if (correct.length === 0) {
+                        ctx.addIssue({
+                            message: `Multiple response questions must have at least one correct answer for question "${question.question_attributes.content}"`,
+                            code: 'invalid_correct_option',
+                        });
+                    }
+                }
+            }),
+            ).superRefine((data, ctx) => {
+                // ensure questions are unique
+                const questions = data.map(question => question.question_attributes.content).flat();
+                const questionSet = new Set();
+                const duplicates = new Set();
+                questions.forEach(question => {
+                    if (questionSet.has(question)) {
+                        duplicates.add(question);
+                    } else {
+                        questionSet.add(question);
+                    }
+                });
+                if (duplicates.size > 0) {
                     ctx.addIssue({
-                        message: `Multiple response questions must have at least one correct answer for question "${question.question_attributes.content}"`,
-                        code: 'invalid_correct_option',
+                        message: `Questions must be unique. Duplicates found: ${Array.from(duplicates).join(', ')}`,
+                        code: 'invalid_questions',
                     });
                 }
-            }
-        }),
-        ).superRefine((data, ctx) => {
-            // ensure questions are unique
-            const questions = data.map(question => question.question_attributes.content).flat();
-            const questionSet = new Set();
-            const duplicates = new Set();
-            questions.forEach(question => {
-                if (questionSet.has(question)) {
-                    duplicates.add(question);
-                } else {
-                    questionSet.add(question);
-                }
-            });
-            if (duplicates.size > 0) {
-                ctx.addIssue({
-                    message: `Questions must be unique. Duplicates found: ${Array.from(duplicates).join(', ')}`,
-                    code: 'invalid_questions',
-                });
-            }
+            })
         })
-    })
-)
-console.log('Validating schema for', fileName);
+    )
+    console.log('Validating schema for', fileName);
 
-try {
-    schema.parse(data);
-} catch (error) {
-    console.error('Schema is invalid:', error.errors);
-    process.exit(1);
-}
+    try {
+        schema.parse(data);
+    } catch (error) {
+        console.error('Schema is invalid for', fileName, ':', error.errors);
+        process.exit(1);
+    }
+});
+
 function parse_args() {
     const args = process.argv.slice(2);
+    if (args.length === 0) {
+        return fs.readdirSync(join(__dirname, 'gists'));
+    }
     if (args.length !== 1) {
         console.error('Usage: node merge_json_data.js <file>');
         process.exit(1);
     }
-    return args[0];
+    return [args[0]];
 }
